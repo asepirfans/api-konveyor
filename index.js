1c2b1c25a0d0843a8e91ebfffab0d16a8f5aed88
Asep Irfan Setiawan <asepirfans.9c@gmail.com>
2024-07-06 19:40:59 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/database');
+const sensorRoutes = require('./routes/sensorRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+connectDB();
+
+app.get('/', ()=> {
+  res.send("Api Ready")
+})
+app.use('/api', sensorRoutes);
+app.use('/user', userRoutes);
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Server berjalan di port ${port}`);
+});
