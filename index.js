0589ffcb41853a380d8b72ebb26340426064adf2
Asep Irfan Setiawan <asepirfans.9c@gmail.com>
2024-07-16 16:14:08 +0700

    update port

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res)=> {
 app.use('/api', sensorRoutes);
 app.use('/user', userRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const server = app.listen(port, () => {
   console.log(`Server berjalan di port ${port}`);
 });
