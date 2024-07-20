861b8dbbead01436592cb6a29b47860c2b017f5d
Asep Irfan Setiawan <asepirfans.9c@gmail.com>
2024-07-20 22:14:22 +0700

    update be 1.2

--- index.js
+++ index.js
@@ -24,3 +24,25 @@ const port = 3030;
 const server = app.listen(port, () => {
   console.log(`Server berjalan di port ${port}`);
 });
+
+const sensor  = require('./models/sensorModel');
+const verifyToken = require('./middleware/verifyToken');
+
+let statusPlant = false;
+let timer;
+app.get('/kondisiPlant', verifyToken,(req, res) => {
+    res.status(200).json({ status: statusPlant });
+});
+
+const dataChangeStream = sensor.watch();
+
+dataChangeStream.on('change', (change) => {
+    if (change.operationType === 'insert') {
+        statusPlant = true;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            statusPlant = false;
+        }, 10000); 
+    }
+}
+);
